a6b6f210ffe55af79fff9aba7023b542df063ac3
Sahaj Kedia <sahajkedia20152498@gmail.com>
2024-04-23 17:42:31 +0530

    updated the common folder and blog.ts file

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-
+import { createBlogInput } from "@100xdevs/medium-common";
+import { updateBlogInput } from "@100xdevs/medium-common";
 export const blogRoutes = new Hono<{
 	Bindings: {
 		DATABASE_URL: string;
@@ -18,17 +19,25 @@ blogRoutes.post("/", async (c) => {
 		datasourceUrl: c.env.DATABASE_URL,
 	}).$extends(withAccelerate());
 	const { title, content } = await c.req.json();
-	const post = await prisma.post.create({
-		data: {
-			title,
-			content,
-			authorId: userId,
-		},
-	});
+	const { success } = createBlogInput.safeParse({ title, content });
+	if (success) {
+		const post = await prisma.post.create({
+			data: {
+				title,
+				content,
+				authorId: userId,
+			},
+		});
 
-	return c.json({
-		id: post.id,
-	});
+		return c.json({
+			id: post.id,
+		});
+	} else {
+		c.status(403);
+		return c.json({
+			msg: " Unsuccessful because Input Validation Failed",
+		});
+	}
 });
 blogRoutes.put("/", async (c) => {
 	const userId = c.get("userId");
@@ -36,18 +45,26 @@ blogRoutes.put("/", async (c) => {
 		datasourceUrl: c.env.DATABASE_URL,
 	}).$extends(withAccelerate());
 	const { title, content, id } = await c.req.json();
-	await prisma.post.update({
-		where: {
-			id: id,
-			authorId: userId,
-		},
-		data: {
-			title: title,
-			content: content,
-		},
-	});
+	const { success } = createBlogInput.safeParse({ title, content, id });
+	if (success) {
+		await prisma.post.update({
+			where: {
+				id: id,
+				authorId: userId,
+			},
+			data: {
+				title: title,
+				content: content,
+			},
+		});
 
-	return c.text("Post Updated");
+		return c.text("Post Updated");
+	} else {
+		c.status(403);
+		return c.json({
+			msg: "Update Unsuccessful",
+		});
+	}
 });
 
 // Add Pagination
